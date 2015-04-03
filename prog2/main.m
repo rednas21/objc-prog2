50d1ee8ce0ce9c6fa0f69dbaa547ab830fd215ac
Sander Peerna <sanderpeerna@gmail.com>
2015-04-03 14:24:20 -0700

    Initial Commit

---
+++ prog2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog2
+//
+//  Created by Sander Peerna on 4/3/15.
+//  Copyright (c) 2015 Sander Peerna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
